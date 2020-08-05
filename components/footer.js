d7872214f489ba482c0c12e07956bce7e4abc547
Roger Perren <info@blueforce.ch>
2020-08-05 15:19:17 +0200

    Anpassung Footer

--- components/footer.js
+++ components/footer.js
@@ -11,9 +11,9 @@ export default function Footer() {
           </h3>
           <div className="flex flex-col lg:flex-row justify-center items-center lg:pl-4 lg:w-1/2">
             <a
-              href="https://datocms.com"
+              href={`https://datocms.com`}
               className="mx-3 bg-black hover:bg-white hover:text-black border border-black text-white font-bold py-3 px-12 lg:px-8 duration-200 transition-colors mb-6 lg:mb-0"
-              target="_blank"
+              target={`_blank`}
             >
               DatoCMS
             </a>
