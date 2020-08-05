bf91315c0445d275bf72bf7a9ae5ba159b6e3a8b
Roger Perren <info@blueforce.ch>
2020-08-05 13:26:21 +0200

    Update date.js
    Update

--- components/date.js
+++ components/date.js
@@ -2,5 +2,5 @@ import { parseISO, format } from 'date-fns'
 
 export default function Date({ dateString }) {
   const date = parseISO(dateString)
-  return <time dateTime={dateString}>{format(date, 'dd. F yyyy')}</time>
+  return <time dateTime={dateString}>{format(date, 'dd. M yyyy')}</time>
 }
