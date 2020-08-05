835af5cd09484503add8b3222920edf3d95865cb
Roger Perren <info@blueforce.ch>
2020-08-05 14:29:07 +0200

    Update footer.js
    Anpassungen

--- components/footer.js
+++ components/footer.js
@@ -11,20 +11,12 @@ export default function Footer() {
           </h3>
           <div className="flex flex-col lg:flex-row justify-center items-center lg:pl-4 lg:w-1/2">
             <a
-              href="https://nextjs.org/docs/basic-features/pages"
-              className="mx-3 bg-black hover:bg-white hover:text-black border border-black text-white font-bold py-3 px-12 lg:px-8 duration-200 transition-colors mb-6 lg:mb-0"
-            >
-              Zur Dokumentation
-            </a>
-            <a
               href={`https://github.com/blueforce/datocms-blog-demo/`}
               className="mx-3 font-bold hover:underline"
             >
               Auf GitHub anschauen
             </a>
-          </div>
-          <div className="flex flex-col lg:flex-row justify-center items-center lg:pl-4 lg:w-1/1">
-             <p>©2019 Made with ♡ by Blueforce enpowered with <a href="https://datocms.com">DatoCMS</a></p>
+            ©2019 Made with ♡ by Blueforce enpowered with <a href="https://datocms.com">DatoCMS</a>
           </div>
         </div>
       </Container>
