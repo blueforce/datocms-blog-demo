8197fccb7dc5525123a6dbc1d840732e21df5e55
Roger Perren <info@blueforce.ch>
2020-08-05 15:16:44 +0200

    Update footer.js
    Redesign

--- components/footer.js
+++ components/footer.js
@@ -1,5 +1,5 @@
 import Container from './container'
-import { EXAMPLE_PATH } from '../lib/constants'
+import { EXAMPLE_PATH } from '@/lib/constants'
 
 export default function Footer() {
   return (
@@ -7,29 +7,24 @@ export default function Footer() {
       <Container>
         <div className="py-28 flex flex-col lg:flex-row items-center">
           <h3 className="text-4xl lg:text-5xl font-bold tracking-tighter leading-tight text-center lg:text-left mb-10 lg:mb-0 lg:pr-4 lg:w-1/2">
-            Erstellt auf Basis von Next.js.
+            ©2020 Made with ♡ by Blueforce enpowered with.
           </h3>
           <div className="flex flex-col lg:flex-row justify-center items-center lg:pl-4 lg:w-1/2">
             <a
-              href={`https://github.com/blueforce/datocms-blog-demo/`}
+              href="https://datocms.com"
+              className="mx-3 bg-black hover:bg-white hover:text-black border border-black text-white font-bold py-3 px-12 lg:px-8 duration-200 transition-colors mb-6 lg:mb-0"
+              target="_blank"
+            >
+              DatoCMS
+            </a>
+            <a
+              href={`https://github.com/blueforce/datocms-blueforce-blog`}
               className="mx-3 font-bold hover:underline"
             >
               Auf GitHub anschauen
             </a>
           </div>
         </div>
-          <div className="py-2 flex flex-col lg:flex-row items-center">
-          <div className="flex justify-center text-center">
-          ©2020 Made with ♡ by Blueforce enpowered with {' '}
-        <a
-          href="https://datocms.com"
-          target="_blank"
-          className="underline hover:text-success duration-200 transition-colors"
-        >
-          DatoCMS
-        </a>{' '}
-           </div>
-          </div>
       </Container>
     </footer>
   )
