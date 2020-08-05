11c1ff74e904511df38bb5ce1b9b4138167d52de
Roger Perren <info@blueforce.ch>
2020-08-05 15:36:11 +0200

    Anpassung des Footers

--- components/footer.js
+++ components/footer.js
@@ -26,10 +26,10 @@ export default function Footer() {
         </div>
         <div className="py-2 flex flex-col lg:flex-row items-center">
           <div className="flex flex-col lg:flex-row justify-center items-center lg:pl-4 lg:w-1/2">
-          ©2020 Made with ♡ by Blueforce enpowered with
+          © 2020 Made with ♡ by Blueforce enpowered with
             <a
               href={`https://datocms.com`}
-              className="mx-3 bg-black hover:bg-white hover:text-black border border-black text-white font-bold py-3 px-12 lg:px-8 duration-200 transition-colors mb-6 lg:mb-0"
+              className="underline hover:text-success duration-200 transition-colors"
               target={`_blank`}
             >
               DatoCMS
