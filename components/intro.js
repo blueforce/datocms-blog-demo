222e7176e66f8b4347280c4ac12ebf52ff1695d7
Roger Perren <info@blueforce.ch>
2020-08-05 12:28:49 +0200

    Update intro.js
    Anpassung Template Startseite

--- components/intro.js
+++ components/intro.js
@@ -4,22 +4,22 @@ export default function Intro() {
   return (
     <section className="flex-col md:flex-row flex items-center md:justify-between mt-16 mb-16 md:mb-12">
       <h1 className="text-6xl md:text-8xl font-bold tracking-tighter leading-tight md:pr-8">
-        Blog.
+        Blueforce Blog.
       </h1>
       <h4 className="text-center md:text-left text-lg mt-5 md:pl-8">
-        A statically generated blog example using{' '}
+        Eine Testumgebung für ein nächstes{' '}
         <a
-          href="https://nextjs.org/"
+          href="https://powermedia.ch/"
           className="underline hover:text-success duration-200 transition-colors"
         >
-          Next.js
+          Kundenprojekt
         </a>{' '}
-        and{' '}
+        von{' '}
         <a
-          href={CMS_URL}
+          href="https://www.blueforce.ch"
           className="underline hover:text-success duration-200 transition-colors"
         >
-          {CMS_NAME}
+          Blueforce Internet Solutions
         </a>
         .
       </h4>
